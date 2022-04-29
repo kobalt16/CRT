8d3ed5de16063e2edc700c3f333ce38e70114830
Ruslan (Elifas Levi) <96114211+kobalt16@users.noreply.github.com>
2022-04-29 19:47:09 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//music
+//music====
 let soundButton = document.querySelector('.soundbutton'),
     audio = document.querySelector('.audio')
 
@@ -7,19 +7,26 @@ soundButton.addEventListener('click', e => {
     audio.paused ? audio.play() : audio.pause()
 });
 
-//game
 
-const lexx = document.getElementById("lexx");
+
+
+
+//game
+//create LEXX-------------------------------------------
+
+
+
+let lexx = document.getElementById("lexx");
 
 document.addEventListener("keydown", function(event) {
     
     switch(event.key) {
         case "ArrowUp":
-            lexx.style.top = lexx.offsetTop - 120 + "px";
+            lexx.style.top = lexx.offsetTop - 30 + "px";
             break;
 
         case "ArrowDown":
-            lexx.style.top = lexx.offsetTop + .1 + "px";
+            lexx.style.top = lexx.offsetTop + 30 + "px";
             break;
 
         case " ":
@@ -28,26 +35,104 @@ document.addEventListener("keydown", function(event) {
     }
 });
 
+//fireshot create
+
 function createFireshot() {
     let fireshot = document.createElement("div");
     fireshot.className = "fireshot";
-    fireshot.style.top = lexx.offsetTop + 130 + "px";
-    fireshot.style.left = lexx.offsetLeft + 520 + "px";
-    document.body.appendChild(fireshot);
+
+    fireshot.style.top = lexx.offsetTop + 25 + "px";
+    fireshot.style.left = lexx.offsetLeft + 130 + "px";
+
+    let display = document.getElementById("display-container");
+    let parentDiv = display.parentNode;
+
+    parentDiv.insertBefore(fireshot, display);
     
     fireshotMove(fireshot)
 
     
 }
+createPlanet()
 
 
 function fireshotMove(fireshot) {
     let timerId = setInterval (function() {
-        fireshot.style.left = fireshot.offsetLeft + 10 + "px"
+        let display = document.getElementById("display-container");
+        fireshot.style.left = fireshot.offsetLeft + 5 + "px"
 
-        if(fireshot.offsetLeft > document.body.clientWidth) {
+        isShot(fireshot, timerId);
+
+
+        if(fireshot.offsetLeft > display.clientWidth) {
             fireshot.remove();
             clearInterval(timerId);
         }
-    }, 30);
+    }, 25);
 }
+//isShot
+function isShot(fireshot, timer) {
+    let topF = fireshot.offsetTop;
+    let bottomF = fireshot.offsetTop + fireshot.offsetHeight;
+
+    let planet = document.querySelector(".planet");
+    if(planet != null) {
+        let topP = planet.offsetTop;
+        let bottomP = planet.offsetTop + planet.offsetHeight;
+        
+        let leftF = fireshot.offsetLeft;
+        let leftP = planet.offsetLeft;
+    
+        if(topF >= topP && bottomF <= bottomP && leftF >= leftP) {
+            planet.className = 'boom';
+            planet.style.top = topP + "px";
+            planet.style.left = leftP + "px";
+            clearInterval(planet.dataset.timer);
+            setTimeout(function() {
+                planet.remove();
+                createPlanet();
+                clearInterval(timer);
+            }, 1200)
+        }
+    }
+
+}
+
+
+
+//create Planet
+
+function createPlanet() {
+    let planet = document.createElement("div");
+    planet.className = "planet";
+    
+
+    let display = document.getElementById("display-container");
+    let parentDiv = display.parentNode;
+    parentDiv.insertBefore(planet, display);
+
+    planet.style.top = random(1, display.offsetHeight - 10) + "px";
+
+    
+    let timerId = setInterval(function() {
+
+        planet.style.left = (planet.offsetLeft - 10) + "px";
+
+        
+        if(planet.offsetLeft + planet.offsetWidth < 100) {
+            planet.remove();
+            clearInterval(timerId);
+            createPlanet();
+        }
+    }, 100);
+    planet.dataset.timer = timerId;
+    
+}
+
+//random 
+function random(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+
